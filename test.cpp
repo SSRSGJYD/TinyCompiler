6171da50bc9a9c81fbb374b9d9478a34c114e00f
maoyutao <maoyutao1006@163.com>
2018-12-29 15:45:58 +0800

    回文检测

--- test.cpp
+++ test.cpp
@@ -1,18 +1,28 @@
 extern int printf(string format);
-extern int puts(string s);
+extern int scanf(string format);
+extern int strlen(string s);
 
-
-
-int func(int a, int b[3]){
-	printf("%d", b[a]);
+int palindrome(char str[],int len){
+    int i;
+    int l = len / 2;
+    len = len - 1;
+    for(i = 0; i <= l; i = i + 1){
+        if(str[i] != str[len - i]){
+            printf("False");
+            i = l + 2;
+        }
+    }
+    if (i == l + 1) {
+        printf("True");
+    }
     return 0;
-}
+} 
 
 int main(){
-	int a[3] = {1, 2, 3};
-    int i;
-    for( i = 1 ; i<3; i=i+1){
-        func(i, a);
-    }
+	char str[128];
+    printf("【回文检测】请输入128字节以内的字符串：");
+    scanf("%s", str);
+    int len = strlen(str);
+    palindrome(str, len);
     return 0;
 }
